c72850d67e0d789eed14e5178a57fbff45ed3cba
josh <urgosxd@gmail.com>
2024-11-29 14:27:51 -0500

    aoe

--- src/core/settings.py
+++ src/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost',
     'http://localhost:1337',
+    'https://paymentserver.pdsviajes.com/',
     'http://XX.XX.XX.XX',
 ]
 # Application definition
